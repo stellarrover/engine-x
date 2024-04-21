dd4c0241dbacbd5f367c840113bce33e54a7933e
Felix <jingfeng@stellarrover.com>
2024-04-21 14:08:26 +0800

    pref: amend

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthProxyModule } from './modules/auth-proxy/auth-proxy.module';
+import { FileUploadModule } from './modules/file-upload/file-upload.module';
 
 @Module({
-  imports: [AuthProxyModule],
-  exports: [AuthProxyModule],
+  imports: [AuthProxyModule, FileUploadModule],
+  exports: [AuthProxyModule, FileUploadModule],
 })
 export class CoreModule {}
